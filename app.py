53c3ed4d2fdd8b7fddece67a8e143907bd59dfa8
proolya <proolya@yandex.ru>
2026-07-06 19:47:44 +0300

    add gitignore

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ print("hello word")
 
 git config --global user.name proolya
 git config --global user.email proolya@yandex.ru
+
+# git status - проверка статуса
+# git add - <название файла> добавление в index
+# git commit -m  "комментарий к комиту"
+# git log - отобразить комиты
